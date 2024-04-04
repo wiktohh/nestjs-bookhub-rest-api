9b78a630cb392d6c0335a7909a0beba493d92c43
wiktohh <wiktor.rzeznicki01@gmail.com>
2024-04-04 06:40:30 +0200

    Fix bug related with ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { BookModule } from './book/book.module';
 import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule, AuthModule, UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule,
+    AuthModule,
+    UserModule,
+    BookModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

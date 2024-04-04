24f828acbceb12e2e09dc9444d64e5daa1fdfd2e
wiktohh <wiktor.rzeznicki01@gmail.com>
2024-04-04 07:07:43 +0200

    Add controllers and providers in BookModule

--- src/book/book.module.ts
+++ src/book/book.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { BookController } from './book.controller';
+import { BookService } from './book.service';
 
-@Module({})
+@Module({
+  controllers: [BookController],
+  providers: [BookService],
+})
 export class BookModule {}
